54212a08aa8a6c0fccc629158ea0668168d50306
Abdelkerim Mahamat Zeni <58027608+abdel-137@users.noreply.github.com>
2019-12-08 14:55:04 +0100

    controleurs.py

--- Controleurs.py
+++ Controleurs.py
@@ -1,24 +1,27 @@
-# classControleurs
+#coding:utf-8
+
+classControleurs
+
 
 def main():
     # creation d'une variable 'compte_bancaire'
+  
     compte = 10000
-    #compte d'un client
     print("compte")
-
+       #compte d'un client
     solde = 10000
     print("solde du client")
 
     taux = 500
-    # taux fixè par la banque
+       # taux fixè par la banque
     print("valeur du solde")
 
     deposer = solde
     print("solde +")
-    # dépot de l'argent
+      # dépot de l'argent
     retirer = solde
     print("solde -")
-    # retré de l'argent
+       # retré de l'argent
 
 if __name__ == '__main__':
     main()
